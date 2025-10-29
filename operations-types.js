2aac6a1902e64ea4e23c86f029dcfe5342813c51
unknown <tamrynripepi00@gmail.com>
2025-10-29 11:26:38 +0200

    add mango

---
+++ operations-types.js
@@ -0,0 +1,11 @@
+let number1 = 5;
+let number2 = 10;
+
+let number1String = String(number1);
+console.log(number1String);
+
+let sum = number1 + number2;
+console.log(sum);
+
+let product = number1 * number2;
+console.log(product);
